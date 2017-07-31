3fba06c8ea8e98b8c89071e7e1bd749a182226f8
Rakesh Mahajan <mrocks07@gmail.com>
2017-07-31 19:26:53 +0530

    Add files via upload

--- led.js
+++ led.js
@@ -2,5 +2,7 @@ var Gpio = require('onoff').Gpio,
     led = new Gpio(4, 'out');  
 var iv = setInterval(function ()  
 {  
-    led.writeSync(led.readSync() === 0 ? 1 : 0)  
-}, 10000);  
+    
+led.writeSync(led.readSync() === 0 ? 1 : 0)  
+}, 1000); 
+ 
