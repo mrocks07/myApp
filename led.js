adff85295a783e73a2f0f545e92fc8edcc8628d3
Rakesh Mahajan <mrocks07@gmail.com>
2017-08-01 11:24:12 +0530

    Add files via upload

--- led.js
+++ led.js
@@ -1,8 +1,18 @@
 var Gpio = require('onoff').Gpio,  
     led = new Gpio(4, 'out');  
+
 var iv = setInterval(function ()  
 {  
     
 led.writeSync(led.readSync() === 0 ? 1 : 0)  
 }, 1000); 
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Welcome to firmware upgrade test app!');
+}).listen(8081);
+
+
  
