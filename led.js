b8bea804b1fa2fb03552757b118f5177ecf5debd
Rakesh Mahajan <mrocks07@gmail.com>
2017-08-02 18:08:08 +0530

    Update led.js

--- led.js
+++ led.js
@@ -12,8 +12,8 @@ var http = require('http');
 var server=http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Welcome to firmware upgrade Appllication3! ');
-    console.log('Server running at http://192.168.0.52:5051/');
-}).listen(5051);
+    console.log('Server running at http://192.168.0.52:5052/');
+}).listen(5052);
 
 server.once('error', function(err) 
    {
